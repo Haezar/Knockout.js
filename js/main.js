e658053fb9cb11ea34340083130273e47994a2c7
Haezar <mrcatby.mg@gmail.com>
2019-08-20 19:11:09 +0300

    Step 3

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
 function ContributorsViewModel() {
   let self = this;
   self.filters = ['all', 'gold', 'silver', 'bronze'];
+  self.sorters = ['asc', 'desc'];
   self.contributors = ko.observableArray();
   self.chosenFilter = ko.observable('all');
+  self.chosenSorting = ko.observable();
   getContributors().then(function(response) {
     response.forEach(element => {
       let contributor = element;
@@ -17,6 +19,16 @@ function ContributorsViewModel() {
   self.setFilter = function(filter) {
     self.chosenFilter(filter);
   }
+  self.setSort = function(sort) {
+    self.chosenSorting(sort);
+    if(sort=="asc"){
+      self.contributors(self.contributors().sort(contributorsSortingASC))
+    }
+    else {
+      self.contributors(self.contributors().sort(contributorsSortingDESC))
+    }
+  }
+
 }
 async function getContributors(){
   let contributors = await fetch('https://api.github.com/repos/thomasdavis/backbonetutorials/contributors')
@@ -24,4 +36,23 @@ async function getContributors(){
     return response.json();
   });
   return contributors;
+}
+
+function contributorsSortingDESC(firstContributor, secondContributor){
+  if (firstContributor.login.toLowerCase() < secondContributor.login.toLowerCase()) {
+    return  1
+  }
+  if (firstContributor.login.toLowerCase() > secondContributor.login.toLowerCase()) {
+    return -1
+  }
+  return 0
+}
+function contributorsSortingASC(firstContributor, secondContributor){
+  if (firstContributor.login.toLowerCase() < secondContributor.login.toLowerCase()) {
+    return -1
+  }
+  if (firstContributor.login.toLowerCase() > secondContributor.login.toLowerCase()) {
+    return  1
+  }
+  return 0
 }
